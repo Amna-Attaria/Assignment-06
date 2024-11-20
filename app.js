d3319de8849e6d9ccbbed0405f58d36975cac249
Amna-Attaria <khan12amna26@gmail.com>
2024-11-20 17:34:04 +0500

    JS Assignment Chapter: 26-30

---
+++ app.js
@@ -0,0 +1,67 @@
+// Qno:01
+// var num = prompt("ENTER Positive Integers Number");
+// var round = Math.round(num);
+// var floor = Math.floor(num);
+// var ceil = Math.ceil(num);
+// document.write("number: " + num + "<br>" + "round off value" + round + "<br>" + "floor Value " + floor + "<br>" + "ceil value: " + ceil );
+
+// Qno:02
+// var num = prompt("ENTER negative floating point");
+// var round = Math.round(num);
+// var floor = Math.floor(num);
+// var ceil = Math.ceil(num);
+// document.write("number: " + num + "<br>" + "round off value" + round + "<br>" + "floor Value " + floor + "<br>" + "ceil value: " + ceil );
+
+// Qno:03
+// var number = prompt("Enter a number")
+// number = Number(number)
+// var absoluteVlaue;
+// if(number<0)
+//     {
+//        absoluteVlaue = -number
+//     }
+//     else{
+//         absoluteVlaue = number
+//     }
+//     document.write(`The absolute value of  ${number} is  ${absoluteVlaue}`)
+
+// Qno:04
+// var dice = Math.floor(Math.random()*6)+1
+// document.write("The Value Of Dice Is " + dice);
+
+// Qno:05
+// var tail = prompt("Enter Tails Name");
+// var head = prompt("Enter Head Name");
+// var toss = Math.floor(Math.random()*2)
+// if(toss==1)
+//     {
+//         document.write( tail + " is Win")
+//     }
+//     else
+//     {
+//         document.write( head + " is Win")
+//     }
+// Qno:06
+// var random = Math.floor(Math.random()*100)+1
+// document.write("Random num between 1 and 100 is " + random)
+// Qno:07
+// var weight = prompt("Enter Your Weight")
+// var weights = parseFloat(weight)
+// if (!isNaN(weight))
+//     {
+//         document.write("Your Weight is " + weight );
+//     }
+//     else{
+//         document.write("Invalid input. Please enter a valid number for your weight.");
+//     }
+// Qno:08
+// var secrateNum = 6;
+// var num = prompt("Enter Secrate Between 1 and 10 Num");
+// if(num==secrateNum)
+//     {
+//        alert("You Win")
+//     }
+// else
+// {
+//     alert("Try Again")
+// }
